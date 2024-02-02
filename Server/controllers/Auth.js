9a8fadb5767be8f59ac7572c78115446430cd4a2
infinite-runtime-error <infiniteruntimeerror@gmail.com>
2024-02-02 23:58:12 +0530

    bcrypt set done

--- Server/controllers/Auth.js
+++ Server/controllers/Auth.js
@@ -1,6 +1,6 @@
 const OTP = require('../models/OTP');
 const User = require('../models/User');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const Profile = require('../models/Profile');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
