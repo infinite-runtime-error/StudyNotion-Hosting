fd08326326653e270356fb96db4a48a3830d0618
infinite-runtime-error <144599288+infinite-runtime-error@users.noreply.github.com>
2024-02-03 21:47:29 +0530

    Update  Core index.js

--- Server/index.js
+++ Server/index.js
@@ -34,7 +34,9 @@ app.use(cookieParser());
 // );
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://study-notion-hosting-frontend-cb61fas2o.vercel.app');
+  //res.header('Access-Control-Allow-Origin', 'https://study-notion-hosting-frontend-cb61fas2o.vercel.app');
+  res.header('Access-Control-Allow-Origin', 'https://study-notion-hosting-f-git-e9c595-jatin-ranas-projects-f3e8c072.vercel.app/');
+  
   https://study-notion-hosting-frontend-cb61fas2o.vercel.app/
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
