6e9b2d0ad55525cf95677d4b45b0fb8eebf58e1c
infinite-runtime-error <144599288+infinite-runtime-error@users.noreply.github.com>
2024-02-04 18:03:18 +0000

    Fixing Cors

--- Server/index.js
+++ Server/index.js
@@ -22,81 +22,16 @@ database();
 // middlewares
 app.use(express.json());
 app.use(cookieParser());
-// app.use(
-//     cors({
-//         origin:"https://study-notion-hosting-frontend-5o7e1odqe.vercel.app/",
-//         credentials:true
-//     })
+app.use(
+    cors({
+        origin:"https://study-notion-hosting-frontend-theta.vercel.app/",
+        credentials:true
+     })
 
   
   
 
-// );
-
-// app.use((req, res, next) => {
-//   //res.header('Access-Control-Allow-Origin', 'https://study-notion-hosting-frontend-cb61fas2o.vercel.app');
-//   res.header('Access-Control-Allow-Origin', 'https://study-notion-hosting-f-git-e9c595-jatin-ranas-projects-f3e8c072.vercel.app/');
- 
-//   https://study-notion-hosting-frontend-cb61fas2o.vercel.app/
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//   next();
-// });
-
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'https://study-notion-hosting-f-git-e9c595-jatin-ranas-projects-f3e8c072.vercel.app');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//   res.header('Access-Control-Allow-Credentials', 'true');
-
-//   if (req.method === 'OPTIONS') {
-//     return res.sendStatus(200);
-//   }
-
-//   next();
-// });
-
-
-
-
-
-
-// List of allowed origins
-// const allowedOrigins = [
-//   'https://study-notion-hosting-frontend-theta.vercel.app/',
-//   'https://study-notion-hosting-f-git-e9c595-jatin-ranas-projects-f3e8c072.vercel.app/',
-//   'https://study-notion-hosting-frontend-jqhqym8ua.vercel.app/'
-  // Add more origins as needed
-// ];
-
-// Add this middleware to handle CORS
-app.use((req, res, next) => {
-  // const origin = req.headers.origin;
-
-  // Check if the request origin is in the allowedOrigins array
-  if (allowedOrigins.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-
-    if (req.method === 'OPTIONS') {
-      return res.sendStatus(200);
-    }
-  }
-
-  next();
-});
-
-
-
-
-
-
-
-
-
-
+);
 
 
 app.use(fileUpload({
